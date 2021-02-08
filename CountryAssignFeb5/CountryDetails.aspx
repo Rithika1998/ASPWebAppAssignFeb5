83a334d20f78b500e235bcdd7aa9cd97596295f7
Rithika Namburi <rithika.namburi@zensar.com>
2021-02-08 20:05:46 +0530

    Commiting Country Asignment Files

---
+++ CountryAssignFeb5/CountryDetails.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CountryDetails.aspx.cs" Inherits="CountryAssignFeb5.CountryDetails" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            &nbsp;<br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblCountry" runat="server" Text="Country Id"></asp:Label>
+&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownCountry" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownCountry_SelectedIndexChanged">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblStat" runat="server" Text="State Id"></asp:Label>
+&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownState" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownState_SelectedIndexChanged">
+            </asp:DropDownList>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblCity" runat="server" Text="City Id"></asp:Label>
+&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownCity" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownCity_SelectedIndexChanged">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ CountryAssignFeb5/CountryDetails.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CountryAssignFeb5
+{
+    public partial class CountryDetails : System.Web.UI.Page
+    {
+        private SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings[1].ToString());
+        
+        //--------- COUNTRY DETAILS --------------
+
+        public void CountryDet()
+        {
+            cn.Open();
+            using (SqlCommand cmd = new SqlCommand("select CountryName,CountryId from Country", cn))
+            {
+                using (SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    DropDownCountry.DataSource = dr;
+                    DropDownCountry.Items.Clear();
+                    
+                    DropDownCountry.DataTextField = "CountryName";
+                    DropDownCountry.DataValueField = "CountryId";
+                    DropDownCountry.DataBind();
+
+                }
+            }
+        }
+
+        //--------- STATE DETAILS --------------
+
+        public void StateDet()
+        {
+            cn.Open();
+            //Response.Write(DropDownCountry.SelectedValue);
+            using (SqlCommand cmd = new SqlCommand("select StateName,StateId from Statetable where CountryId='" + DropDownCountry.SelectedValue + "'", cn))
+            {
+                using (SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    DropDownState.DataSource = dr;
+                    DropDownState.Items.Clear();
+                   
+                    DropDownState.DataTextField = "StateName";
+                    DropDownState.DataValueField = "StateId";
+                    DropDownState.DataBind();
+                }
+            }
+        }
+
+        //--------- CITY DETAILS --------------
+
+        public void CityDet()
+        {
+            cn.Open();
+            using (SqlCommand cmd = new SqlCommand("select CityName,CityId from CityTable where StateId='" + DropDownState.SelectedValue + "'", cn))
+            {
+                using (SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    DropDownCity.DataSource = dr;
+                    DropDownCity.Items.Clear();
+                    
+                    DropDownCity.DataTextField = "CityName";
+                    DropDownCity.DataValueField = "CityId";
+                    DropDownCity.DataBind();
+                }
+            }
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CountryDet();
+
+            }
+        }
+
+        protected void DropDownCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblCountry.Text = DropDownCountry.SelectedValue;
+            StateDet();
+        }
+
+        protected void DropDownState_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblStat.Text= DropDownState.SelectedValue;
+            CityDet();
+        }
+
+        protected void DropDownCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblCity.Text = DropDownCity.SelectedValue;
+        }
+    }
+}
